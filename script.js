45ac2cc7673049a0e55484d2db307abb3857d3d0
Ogonick <ogonick1@gmail.com>
2024-11-24 23:39:22 +0200

    ad contacts page and din

--- script.js
+++ script.js
@@ -20,3 +20,27 @@ document.querySelectorAll('.accordion-header').forEach(button => {
     }
   });
 });
+
+
+document.querySelectorAll('.header__nav-item > a').forEach(link => {
+  link.addEventListener('click', event => {
+    const menu = link.nextElementSibling;
+    if (menu && menu.classList.contains('dropdown-menu')) {
+      event.preventDefault();
+      menu.classList.toggle('open');
+      link.querySelector('.arrow').classList.toggle('rotate');
+    }
+  });
+});
+
+// Закриття меню при кліку поза ним
+document.addEventListener('click', event => {
+  if (!event.target.closest('.header__nav-item')) {
+    document.querySelectorAll('.dropdown-menu').forEach(menu => {
+      menu.classList.remove('open');
+    });
+    document.querySelectorAll('.arrow').forEach(arrow => {
+      arrow.classList.remove('rotate');
+    });
+  }
+});
